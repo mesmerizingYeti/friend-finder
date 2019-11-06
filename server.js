9ac4d01c3c3e7cb6a2d31e679d4f0e14d1882ac6
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-11-06 01:07:32 -0800

    Added express, created basic server and home.html

---
+++ server.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+
+app.listen(3000, () => console.log('Server listening on port 3000.'))
+
+app.use(express.static(path.join(__dirname, 'app', 'public')))
+app.use(express.json())
+app.use(express.urlencoded({  extended: true }))
+
